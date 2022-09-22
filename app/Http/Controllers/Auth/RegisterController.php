ea7d73342d5739858f556c3f71ecd10430637979
SubarnaTwayana <subarna.twayana@tai.com.np>
2022-09-22 10:24:25 +0545

    [add] Registering users

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function index()
+    {
+        return view('auth.register');
+    }
+    public function store(Request $request)
+    {
+        // dd($request->email);
+        $this->validate($request,[
+            'name' => 'required|max:225',
+            'username' => 'required|max:225',
+            'email' => 'required|max:225',
+            'password' => 'required|confirmed',
+        ]);
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' =>Hash::make($request->password),
+        ]);
+
+        return redirect()->route('dashboard');
+        //validate
+         //store user
+        //sign the user in
+        //redirect
+    }
+}
